0ab4a7e99857f31d20a48d73b282e6fb9323f570
Alex Cluff <alexcluff16@gmail.com>
2022-02-16 22:17:28 -0500

    build 3.1

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 # need wine32 with python 3.8 installed
+
 wine pip install -r requirements.txt
 wine pyinstaller --onefile --add-data 'C:\\users\\alex\\Local Settings\\Application Data\\Programs\\Python\\Python38-32\\Lib\\site-packages\\pyfiglet;./pyfiglet' pyordle.py
+
+if [ -n "$1" ]; then
+  mv dist/pyordle.exe "dist/pyordle_v$1.exe"
+else
+  mv dist/pyordle.exe dist/pyordle_v0.exe
+fi
