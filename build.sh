b26ec69470f8d4e7a6f1a0b4668ae9e5a4efcf13
Alex Cluff <alexcluff16@gmail.com>
2022-02-19 16:34:07 -0500

    update packages list first

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 # need wine with python 3.8 + path installed
 
 if ! command -v wine &> /dev/null; then
+    sudo apt update
     sudo apt install wine
     read -p "remember to checkmark 'add path', enter to continue" asdf
     wget https://www.python.org/ftp/python/3.8.8/python-3.8.8.exe
